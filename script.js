162c2d7e8acb8c325f8cf14053c702fabdd8075d
Rahul Muddasani <118585444+Rahulmuddasani123@users.noreply.github.com>
2023-07-20 16:11:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+function c1() {
+    document.getElementById('main-cont').style.backgroundColor = '#fab9b9'
+    document.getElementById('update-color').textContent = "#fab9b9"
+
+}
+
+function c2() {
+    document.getElementById('main-cont').style.backgroundColor = '#d2fab9'
+    document.getElementById('update-color').textContent = "#d2fab9"
+
+}
+
+function c3() {
+    document.getElementById('main-cont').style.backgroundColor = '#c8b9fa'
+    document.getElementById('update-color').textContent = "#c8b9fa"
+
+}
+
+function c4() {
+    document.getElementById('main-cont').style.backgroundColor = '#f2705c'
+    document.getElementById('update-color').textContent = "#fab9b9"
+}
+
+function c5() {
+    document.getElementById('main-cont').style.backgroundColor = '#5cf2eb'
+    document.getElementById('update-color').textContent = "#5cf2eb"
+}
+
+function c6() {
+    document.getElementById('main-cont').style.backgroundColor = '#f57dd9'
+    document.getElementById('update-color').textContent = "#f57dd9"
+}
+
+function c7() {
+    document.getElementById('main-cont').style.backgroundColor = '#3ef08b'
+    document.getElementById('update-color').textContent = "#3ef08b"
+}
+
+function c8() {
+    document.getElementById('main-cont').style.backgroundColor = '#4a8cc7'
+    document.getElementById('update-color').textContent = "#4a8cc7"
+}
+
+function c9() {
+    document.getElementById('main-cont').style.backgroundColor = '#96939e'
+    document.getElementById('update-color').textContent = "#96939e"
+}
+
+function c10() {
+    document.getElementById('main-cont').style.backgroundColor = '#80bab4'
+    document.getElementById('update-color').textContent = "#80bab4"
+}
+
+function c11() {
+    document.getElementById('main-cont').style.backgroundColor = '#fcfaa9'
+    document.getElementById('update-color').textContent = "#fcfaa9"
+}
+
+function c12() {
+    document.getElementById('main-cont').style.backgroundColor = '#ebe9e6'
+    document.getElementById('update-color').textContent = "#ebe9e6"
+}
